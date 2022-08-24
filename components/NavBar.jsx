5d22e44a43ca9e98ed1cd9be88d0aa0b21502f93
Sarah Alzmammi <s.zmammi@outlook.com>
2022-08-25 00:23:33 +0300

    add some changes

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
-
+import Image from 'next/Image'
+import logo from '../public/Frame.png'
 const NavBar = () => {
     
     return (
-        <div className='absolute w-full z-10 ease-in duration-300'>
-            <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
-                
-                <img src='../Frame.png'/> {/* This is logo */}
+        <div className=' w-full '>
+            <div className='  flex justify-between items-center p-4 text-white'>
+                <Image src={logo} width={ 150} height={ 150 } alt='logo' />
+         
                 <button className="px-4 rounded-full bg-yellow-400 text-black">Sign Up</button>
             </div>
         </div>
